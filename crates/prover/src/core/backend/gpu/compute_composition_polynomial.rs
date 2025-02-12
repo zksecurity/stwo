c9a304443b53dbbf55489e57105bca3a990d7540
Jaehun Kim <jaehunkim@outlook.com>
2025-02-12 15:40:32 +0900

    fix: remove unnecessary codes from compute_composition_polynomial(2)

--- crates/prover/src/core/backend/gpu/compute_composition_polynomial.rs
+++ crates/prover/src/core/backend/gpu/compute_composition_polynomial.rs
@@ -32,32 +32,10 @@ pub const THREADS_PER_WORKGROUP: u32 = 256;
 pub const N_HALF_FULL_ROUNDS: u32 = 4;
 pub const N_PARTIAL_ROUNDS: u32 = 14;
 
-pub const N_LINE_TWIDDLES_SIZE: u32 = 2048;
-pub const N_CIRCLE_TWIDDLES_SIZE: u32 = N_LINE_TWIDDLES_SIZE * 2;
-
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct GpuExtendedColumn {
     pub data: [[GpuM31; N_LANES as usize]; N_EXTENDED_ROWS as usize],
-    pub length: u32,
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(C)]
-pub struct Twiddles {
-    circle_twiddles: [u32; N_CIRCLE_TWIDDLES_SIZE as usize],
-    circle_twiddles_size: u32,
-    line_twiddles_flat: [u32; N_LINE_TWIDDLES_SIZE as usize],
-    line_twiddles_layer_count: u32,
-    line_twiddles_sizes: [u32; N_LINE_TWIDDLES_SIZE as usize],
-    line_twiddles_offsets: [u32; N_LINE_TWIDDLES_SIZE as usize],
-    mod_inv: u32,
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(C)]
-pub struct GpuOriginalColumn {
-    pub data: [[GpuM31; N_LANES as usize]; N_ORIGINAL_ROWS as usize],
     pub length: u32,
 }
 
@@ -74,23 +52,6 @@ impl From<&CircleEvaluation<CpuBackend, M31, BitReversedOrder>> for GpuExtendedC
         GpuExtendedColumn {
             data,
             length: N_EXTENDED_ROWS,
-        }
-    }
-}
-
-impl From<&CircleEvaluation<CpuBackend, M31, BitReversedOrder>> for GpuOriginalColumn {
-    fn from(value: &CircleEvaluation<CpuBackend, M31, BitReversedOrder>) -> Self {
-        let mut data = [[GpuM31 { data: 0 }; N_LANES as usize]; N_ORIGINAL_ROWS as usize];
-        for (i, chunk) in value.values.chunks(N_LANES as usize).enumerate() {
-            let mut row = [GpuM31 { data: 0 }; N_LANES as usize];
-            for (j, &val) in chunk.iter().enumerate() {
-                row[j] = val.into();
-            }
-            data[i] = row;
-        }
-        GpuOriginalColumn {
-            data,
-            length: N_ORIGINAL_ROWS,
         }
     }
 }
--- crates/prover/src/core/backend/gpu/prove.rs
+++ crates/prover/src/core/backend/gpu/prove.rs
@@ -95,9 +95,6 @@ async fn evaluate_constraint_quotients_on_domain_gpu<'a, E: FrameworkEval + Any 
         CanonicCoset::new(component.max_constraint_log_degree_bound()).circle_domain();
     let trace_domain = CanonicCoset::new(component.eval().log_size());
 
-    println!("trace_domain: {:?}", trace_domain.log_size());
-    println!("eval_domain: {:?}", eval_domain.log_size());
-
     let mut component_polys = trace.polys.sub_tree(&component.trace_locations());
     component_polys[PREPROCESSED_TRACE_IDX] = component
         .preproccessed_column_indices()
@@ -119,13 +116,6 @@ async fn evaluate_constraint_quotients_on_domain_gpu<'a, E: FrameworkEval + Any 
         .iter()
         .flatten()
         .any(|c| c.domain != eval_domain);
-
-    let original_trace = component_evals
-        .clone()
-        .map_cols(|c| Cow::Borrowed(*c))
-        .as_cols_ref()
-        .map_cols(|c| c.to_cpu());
-    let original_trace_cols = original_trace.as_cols_ref();
     let trace: TreeVec<Vec<Cow<'_, CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>>>> =
         if need_to_extend {
             let _span = span!(Level::INFO, "Extension").entered();
@@ -136,14 +126,6 @@ async fn evaluate_constraint_quotients_on_domain_gpu<'a, E: FrameworkEval + Any 
         } else {
             component_evals.clone().map_cols(|c| Cow::Borrowed(*c))
         };
-
-    // // for test purpose
-    // let gpu_trace = extend_trace_gpu(
-    //     original_trace,
-    //     trace_domain.log_size(),
-    //     eval_domain.log_size(),
-    // )
-    // .await;
 
     // Denom inverses.
     let log_expand = eval_domain.log_size() - trace_domain.log_size();
