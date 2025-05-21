1715ba1791376eac3df8eeb0df4cd0a1dd3044ee
Jaehun Kim <jaehunkim@outlook.com>
2025-05-21 12:40:03 +0900

    refactor: fix dependencies

--- crates/prover/src/wasm_multithread/mod.rs
+++ crates/prover/src/wasm_multithread/mod.rs
@@ -2,21 +2,9 @@
 pub use wasm_bindgen_rayon::init_thread_pool;
 
 #[cfg(all(feature = "parallel", target_family = "wasm", not(target_os = "wasi")))]
-fn get_hardware_concurrency() -> usize {
-    web_sys::window()
-        .map(|w| w.navigator().hardware_concurrency() as usize)
-        .unwrap_or_else(|| {
-            web_sys::console::warn_1(
-                &"navigator.hardwareConcurrency unavailable; defaulting to 1".into(),
-            );
-            1
-        })
-}
-
-#[cfg(all(feature = "parallel", target_family = "wasm", not(target_os = "wasi")))]
-pub async fn rayon_init_thread_pool() {
-    let num_threads = get_hardware_concurrency();
+async fn rayon_init_thread_pool(num_threads: usize) {
     let promise = init_thread_pool(num_threads);
+    // Alternatively, num_threads can be set dynamically using hardware_concurrency()
     if let Err(err) = wasm_bindgen_futures::JsFuture::from(promise).await {
         web_sys::console::error_1(&format!("Failed to start pool: {:?}", err).into());
     } else {
@@ -27,7 +15,27 @@ pub async fn rayon_init_thread_pool() {
 }
 
 #[cfg(all(target_family = "wasm", not(target_os = "wasi")))]
-pub async fn init_wasm_mt() {
+pub async fn init_wasm_mt(num_threads: usize) {
     #[cfg(feature = "parallel")]
-    rayon_init_thread_pool().await;
+    rayon_init_thread_pool(num_threads).await;
+
+    #[cfg(not(feature = "parallel"))]
+    web_sys::console::log_1(
+        &format!(
+            "Parallel feature is not enabled, num_threads: {} will be ignored",
+            num_threads
+        )
+        .into(),
+    );
+}
+
+#[cfg(all(target_family = "wasm", not(target_os = "wasi")))]
+pub fn wasm_timer_now() -> f64 {
+    web_sys::window()
+        .and_then(|win| win.performance())
+        .map(|p| p.now())
+        .unwrap_or_else(|| {
+            web_sys::console::warn_1(&"window.performance.now() unavailable; using 0.0".into());
+            0.0
+        })
 }
