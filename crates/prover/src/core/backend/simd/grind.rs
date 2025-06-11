22fee1b05973879771c229bb3e2863559fd4bb0a
Jaehun Kim <jaehunkim@outlook.com>
2025-06-12 00:22:39 +0900

    fix: broken build

--- crates/prover/src/core/backend/simd/grind.rs
+++ crates/prover/src/core/backend/simd/grind.rs
@@ -91,7 +91,7 @@ impl GrindOps<Poseidon252Channel> for SimdBackend {
 #[cfg(test)]
 mod tests {
 
-    #[cfg(feature = "parallel")]
+    #[cfg(all(not(target_arch = "wasm32"), feature = "parallel"))]
     #[test]
     fn test_grind_blake_is_determinstic() {
         use itertools::Itertools;
